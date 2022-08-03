3c1efa2d71377eee08559ed237d50a4dbae39e46
maiquocthinh <majquocthjnh@gmail.com>
2022-08-03 08:37:17 +0700

    update box mini info when hover

--- assets/js/main.js
+++ assets/js/main.js
@@ -71,15 +71,15 @@ $('.xpo-slide-full__list').owlCarousel({
     dotsEach: true,
     items: 1,
 });
-$(document).ready(function() {
-    $(window).scroll(function() {
+$(document).ready(function () {
+    $(window).scroll(function () {
         if ($(this).scrollTop() > 0) {
             $('#scroll-top').fadeIn();
         } else {
             $('#scroll-top').fadeOut();
         }
     });
-    $('#scroll-top').click(function() {
+    $('#scroll-top').click(function () {
         $('body,html').animate({
             scrollTop: 0
         }, 400);
@@ -88,7 +88,7 @@ $(document).ready(function() {
 });
 // show hidden menu in mobile
 const menuMobileBtn = document.querySelector(".menu-mobile__btn");
-menuMobileBtn.onclick = function() {
+menuMobileBtn.onclick = function () {
     const headerNavbar = document.querySelector(".header-navbar");
     headerNavbar.classList.toggle('sm-hidden');
     if (headerNavbar.classList.contains('sm-hidden')) {
@@ -101,8 +101,8 @@ menuMobileBtn.onclick = function() {
 };
 // show dropdown-list in mobile
 const navbarItems = document.querySelectorAll(".header-navbar__menu-item.dropdown>a");
-navbarItems.forEach(function(element) {
-    element.onclick = function() {
+navbarItems.forEach(function (element) {
+    element.onclick = function () {
         const dropdownList = this.nextElementSibling;
         const dropdownListOpen = document.querySelector('.dropdown-list:not(.sm-hidden)');
         if (dropdownListOpen && dropdownListOpen !== dropdownList) {
@@ -113,7 +113,7 @@ navbarItems.forEach(function(element) {
 });
 // show search input in mobile
 const searchMobileBtn = document.querySelector(".search-mobile__btn");
-searchMobileBtn.onclick = function() {
+searchMobileBtn.onclick = function () {
     const searchInput = document.querySelector(".header-search");
     searchInput.classList.toggle('sm-hidden');
     if (searchInput.classList.contains("sm-hidden")) {
@@ -126,20 +126,20 @@ searchMobileBtn.onclick = function() {
 };
 // show login/register in mobile
 const userMobileBtn = document.querySelector(".user-mobile__btn");
-userMobileBtn.onclick = function() {
+userMobileBtn.onclick = function () {
     const authModal = document.querySelector(".auth-modal");
     authModal.classList.toggle("hidden");
 };
 // close auth modal
 const authModalCloseBtn = document.querySelector(".modal-dialog__tabs-close");
-authModalCloseBtn.onclick = function() {
+authModalCloseBtn.onclick = function () {
     const authModal = document.querySelector(".auth-modal");
     authModal.classList.add("hidden");
 };
 // change tab active of authModal
 let authModalTabs = document.querySelectorAll(".modal-dialog__tabs-list>li.modal-dialog__tabs-item");
-authModalTabs.forEach(function(element) {
-    element.onclick = function() {
+authModalTabs.forEach(function (element) {
+    element.onclick = function () {
         document.querySelector('.modal-dialog__tabs-item.active').classList.remove("active");
         document.querySelector(".modal-dialog__content>div:not(.hidden").classList.add("hidden");
 
@@ -154,7 +154,7 @@ authModalTabs.forEach(function(element) {
     };
 });
 let forgotUser = document.querySelector(".forgot-user");
-forgotUser.onclick = function() {
+forgotUser.onclick = function () {
     document.querySelector(".modal-dialog__content>div:not(.hidden").classList.add("hidden");
     document.querySelector(".modal-dialog__content .modal-forgot").classList.remove("hidden");
 };
@@ -163,7 +163,7 @@ forgotUser.onclick = function() {
 const btnLogin = document.querySelector("#btn-login");
 const btnRegister = document.querySelector("#btn-register");
 const authModal = document.querySelector(".auth-modal");
-btnLogin.onclick = function() {
+btnLogin.onclick = function () {
     authModal.classList.remove("hidden");
 
     document.querySelector('.modal-dialog__tabs-item.active').classList.remove("active");
@@ -172,7 +172,7 @@ btnLogin.onclick = function() {
     document.querySelector("#modal-dialog__tab-login").classList.add("active");
     document.querySelector(".modal-dialog__content .modal-login").classList.remove("hidden");
 };
-btnRegister.onclick = function() {
+btnRegister.onclick = function () {
     authModal.classList.remove("hidden");
 
     document.querySelector('.modal-dialog__tabs-item.active').classList.remove("active");
@@ -184,10 +184,10 @@ btnRegister.onclick = function() {
 
 // change tab top aside
 const popularPosts = document.querySelectorAll('.tab-container .popular-posts');
-popularPosts.forEach(function(popularPost) {
+popularPosts.forEach(function (popularPost) {
     const asideTopTabs = popularPost.querySelectorAll(".tab-header>li");
-    asideTopTabs.forEach(function(element) {
-        element.onclick = function() {
+    asideTopTabs.forEach(function (element) {
+        element.onclick = function () {
             popularPost.querySelector('.tab-header>li.active').classList.remove("active");
             popularPost.querySelector(".tab-container .tab-content:not(.hidden)").classList.add("hidden");
 
@@ -205,4 +205,46 @@ popularPosts.forEach(function(popularPost) {
         };
     });
 
+});
+
+// fill infoFilm to xpoMiniInfo
+function fillDataToXpoMiniInfo(xpoMiniInfo, infoFilm){
+    if(infoFilm){
+        xpoMiniInfo.querySelector('.xpo-mini-info__name').textContent = infoFilm.dataset.name;
+        xpoMiniInfo.querySelector('.xpo-mini-info__original-name').textContent = infoFilm.dataset.originalName;
+        xpoMiniInfo.querySelector('.xpo-mini-info__imdb').lastChild.textContent = infoFilm.dataset.imdb;
+        xpoMiniInfo.querySelector('.xpo-mini-info__duration').lastChild.textContent = infoFilm.dataset.duration;
+        xpoMiniInfo.querySelector('.xpo-mini-info__year').lastChild.textContent = infoFilm.dataset.year;
+        xpoMiniInfo.querySelector('.xpo-mini-info__short-description').textContent = infoFilm.dataset.shortDescription;
+        xpoMiniInfo.querySelector('.xpo-mini-info__countries').lastChild.textContent = infoFilm.dataset.countries;
+        xpoMiniInfo.querySelector('.xpo-mini-info__categories').lastChild.textContent = infoFilm.dataset.categories;
+        xpoMiniInfo.querySelector('.xpo-mini-info__actors').lastChild.textContent = infoFilm.dataset.actors;
+    }
+}
+// box mini info film: show/hiden when mouse hover, bla bla,....
+const xpoItems = document.querySelectorAll('.xpo-item, .xpo-slide-full__item');
+const xpoMiniInfo = document.querySelector('.xpo-mini-info');
+xpoMiniInfo.onmouseover = function () {
+    xpoMiniInfo.style.display = 'block';
+    xpoMiniInfo.onmousemove = function (event) {
+        xpoMiniInfo.style.left = (-50 + event.pageX) + 'px';
+        xpoMiniInfo.style.top = (20 + event.pageY) + 'px';
+    };
+}
+xpoItems.forEach(function (xpoItem) {
+    // get info of film
+    const infoFilm = xpoItem.querySelector('.xpo-mini-info__data');
+    
+    // handle hiden/show when mouseover/mouseout
+    xpoItem.onmouseover = function () {
+        fillDataToXpoMiniInfo(xpoMiniInfo, infoFilm)
+        xpoMiniInfo.style.display = 'block';
+        xpoItem.onmousemove = function (event) {
+            xpoMiniInfo.style.left = (-50 + event.pageX) + 'px';
+            xpoMiniInfo.style.top = (30 + event.pageY) + 'px';
+        };
+    }
+    xpoItem.onmouseleave = function () {
+        xpoMiniInfo.style.display = 'none';
+    }
 });
